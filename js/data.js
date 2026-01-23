fb264c9e46c2b83493217321ec323c04517c2f95
annavillani149 <anna.villani149@gmail.com>
2026-01-23 23:08:39 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,10 +1,12 @@
 export const items = [
   { 
-   id: "bracciale1", 
+    id: "bracciale1", 
    name: "Bracciale doppia catena snake e grumetta con sfere", 
    price: 18, 
    category: "gioielli", 
-   links: ["https://www.marlu.it/it/bracciali/1803-24287-bracciale-doppia-catena-snake-e-grumetta-con-sfere.html?_gl=1*1fkf28y*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzAkdDE3NjY5NjI1ODkkajYwJGwwJGgw#/121-taglia-regolabile_da_15_a_19_cm/160-colore-oro/177-materiale-pvd_oro"],
+   links: [
+     {url: "https://www.marlu.it/it/bracciali/1803-24287-bracciale-doppia-catena-snake-e-grumetta-con-sfere.html?_gl=1*1fkf28y*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzAkdDE3NjY5NjI1ODkkajYwJGwwJGgw#/121-taglia-regolabile_da_15_a_19_cm/160-colore-oro/177-materiale-pvd_oro", label: "Acquista"}
+   ],
    size: null,
    color: "oro",
    notes: null,
@@ -14,10 +16,25 @@ export const items = [
     name: "Orecchino a cerchio ritorto 1,5 cm",
     price: 14,
     category: "gioielli",
-    links: ["https://www.marlu.it/it/orecchini/2082-31072-orecchino-a-cerchio-ritorto-15-cm.html?_gl=1*yxawkw*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzEkdDE3NjY5NjI3OTckajYwJGwwJGgw#/160-colore-oro/177-materiale-pvd_oro/5757-taglia-diametro_15_cm"],
+    links: [
+      {url: "https://www.marlu.it/it/orecchini/2082-31072-orecchino-a-cerchio-ritorto-15-cm.html?_gl=1*yxawkw*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzEkdDE3NjY5NjI3OTckajYwJGwwJGgw#/160-colore-oro/177-materiale-pvd_oro/5757-taglia-diametro_15_cm", label: "Acquista"}
+    ],
     size: null,
     color: "oro",
     notes: "gioiello venduto singolarmente (7€x2)",
+  },
+  {
+    id: "blush",
+    name: "blush elf",
+    price: 10,
+    category: "makeup",
+    links: [
+      { url: "https://www.douglas.it/it/p/5011252010?variant=1211030&trac=DO_IT.01.01_Shopping.P_Shopping.Google.23065081826..1160910.online.PM&gad_source=1&gad_campaignid=23059071984&gbraid=0AAAAADm5zZOwlHiquiFkm4cQ0p8Dgag5C&gclid=Cj0KCQiA1czLBhDhARIsAIEc7ugAaAJFDjxdjD47KGxuzbonTPStFCKj7xdF536fnlGDp5l9jn6usYwaAiTsEALw_wcB", label: "Douglas"},
+      { url: "https://www.amazon.it/l-f-altamente-pigmentato-delicato-prodotto/dp/B0DFMWPZ72/ref=asc_df_B0DFMX5LDT?mcid=969fe1223f4a33a3b351264f2a72b8bd&tag=googshopit-21&linkCode=df0&hvadid=722184500526&hvpos=&hvnetw=g&hvrand=6221518228208543559&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9050623&hvtargid=pla-2393222454187&hvocijid=6221518228208543559-B0DFMX5LDT-&hvexpln=0&th=1", label:"Amazon"}
+    ],
+    size: null,
+    color: "copper etiquette",
+    notes: null,
   }
 ];
 
