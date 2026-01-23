3a243710bbd74a5b2a212e95dca57bb3a81f418a
annavillani149 <anna.villani149@gmail.com>
2026-01-23 22:17:25 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,42 @@
+export const items = [
+  { 
+   id: "bracciale1", 
+   name: "Bracciale doppia catena snake e grumetta con sfere", 
+   price: 18, 
+   category: "gioielli", 
+   links: ["https://www.marlu.it/it/bracciali/1803-24287-bracciale-doppia-catena-snake-e-grumetta-con-sfere.html?_gl=1*1fkf28y*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzAkdDE3NjY5NjI1ODkkajYwJGwwJGgw#/121-taglia-regolabile_da_15_a_19_cm/160-colore-oro/177-materiale-pvd_oro"],
+   size: null,
+   color: "oro",
+   notes: null,
+  },
+  { 
+    id: "orecchini1",
+    name: "Orecchino a cerchio ritorto 1,5 cm",
+    price: 14,
+    category: "gioielli",
+    links: ["https://www.marlu.it/it/orecchini/2082-31072-orecchino-a-cerchio-ritorto-15-cm.html?_gl=1*yxawkw*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzEkdDE3NjY5NjI3OTckajYwJGwwJGgw#/160-colore-oro/177-materiale-pvd_oro/5757-taglia-diametro_15_cm"],
+    size: null,
+    color: "oro",
+    notes "gioiello venduto singolarmente (7€x2)",
+  }
+];
+
+export const sets = [
+  { 
+    id: "setMarlu1", 
+    title: "Idea set orecchini e bracciale", 
+    itemIds: ["orecchini1","bracciale1"], 
+    targetPrice: 32, 
+    description: "Set di gioielli Marlù formato da orecchini e bracciale dorati" }
+];
+
+/*{
+  id: "",
+  name: "",
+  price: ,
+  category: "",
+  links: [""],
+  size: "",
+  color: "",
+  notes: "",
+  }*/
