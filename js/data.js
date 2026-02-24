449ca16007b87e8823cde685a78d7af8bd02b716
annavillani149 <anna.villani149@gmail.com>
2026-02-24 16:11:45 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,7 +3,8 @@ export const items = [
     id: "bracciale1", 
    name: "Bracciale doppia catena snake e grumetta con sfere", 
    price: 18, 
-   category: "gioielli", 
+   category: "gioielli",
+   image: "https://marlu-18ddd.kxcdn.com/23242-square_large_default/bracciale-doppia-catena-snake-e-grumetta-con-sfere.jpg", 
    links: [
      {url: "https://www.marlu.it/it/bracciali/1803-24287-bracciale-doppia-catena-snake-e-grumetta-con-sfere.html?_gl=1*1fkf28y*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzAkdDE3NjY5NjI1ODkkajYwJGwwJGgw#/121-taglia-regolabile_da_15_a_19_cm/160-colore-oro/177-materiale-pvd_oro", label: "Acquista"}
    ],
@@ -17,6 +18,7 @@ export const items = [
     name: "Orecchino a cerchio ritorto 1,5 cm",
     price: 14,
     category: "gioielli",
+    image: "https://marlu-18ddd.kxcdn.com/26467-square_large_default/orecchino-a-cerchio-ritorto-15-cm.jpg",
     links: [
       {url: "https://www.marlu.it/it/orecchini/2082-31072-orecchino-a-cerchio-ritorto-15-cm.html?_gl=1*yxawkw*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzEkdDE3NjY5NjI3OTckajYwJGwwJGgw#/160-colore-oro/177-materiale-pvd_oro/5757-taglia-diametro_15_cm", label: "Acquista"}
     ],
@@ -30,6 +32,7 @@ export const items = [
     name: "blush elf",
     price: 10,
     category: "makeup",
+    image: "https://m.media-amazon.com/images/I/51LBP+oUIqL._AC_SL1500_.jpg",
     links: [
       { url: "https://www.douglas.it/it/p/5011252010?variant=1211030&trac=DO_IT.01.01_Shopping.P_Shopping.Google.23065081826..1160910.online.PM&gad_source=1&gad_campaignid=23059071984&gbraid=0AAAAADm5zZOwlHiquiFkm4cQ0p8Dgag5C&gclid=Cj0KCQiA1czLBhDhARIsAIEc7ugAaAJFDjxdjD47KGxuzbonTPStFCKj7xdF536fnlGDp5l9jn6usYwaAiTsEALw_wcB", label: "Douglas"},
       { url: "https://www.amazon.it/l-f-altamente-pigmentato-delicato-prodotto/dp/B0DFMWPZ72/ref=asc_df_B0DFMX5LDT?mcid=969fe1223f4a33a3b351264f2a72b8bd&tag=googshopit-21&linkCode=df0&hvadid=722184500526&hvpos=&hvnetw=g&hvrand=6221518228208543559&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9050623&hvtargid=pla-2393222454187&hvocijid=6221518228208543559-B0DFMX5LDT-&hvexpln=0&th=1", label:"Amazon"}
@@ -44,6 +47,7 @@ export const items = [
   name: "Anello a doppia fascia con perla",
   price: 18,
   category: "gioielli",
+  image: "https://marlu-18ddd.kxcdn.com/24032-square_large_default/anello-a-doppia-fascia-con-perla.jpg",  
   links: [{url: "https://www.marlu.it/it/anelli/1915-27033-anello-a-doppia-fascia-con-perla.html#/169-colore-bianco/177-materiale-pvd_oro/3891-taglia-da_15_a_19", label:"Marlù"}],
   size: "da 15 a 19",
   color: "dorato",
@@ -55,6 +59,7 @@ export const items = [
   name: "Collana catena figaro e catena con perle bianche irregolari",
   price: 29,
   category: "gioielli",
+  image: "https://marlu-18ddd.kxcdn.com/24110-square_large_default/collana-catena-figaro-e-catena-con-perle-bianche-irregolari.jpg",  
   links: [{url: "https://www.marlu.it/it/collane/1926-27054-collana-catena-figaro-e-catena-con-perle-bianche-irregolari.html?_gl=1*1bl963f*_up*MQ..*_ga*OTM4Njc1MDk5LjE3NjY5NjA1NDA.*_ga_8ETQBCRR5Q*czE3NjY5NjA1MzkkbzEkZzEkdDE3NjY5NjE3NDIkajYwJGwwJGgw#/169-colore-bianco/177-materiale-pvd_oro", label:"Marlù"}],
   size: null,
   color: "dorato",
@@ -66,6 +71,7 @@ export const items = [
   name: "Collana medaglietta sole pendente con zirconi",
   price: 23,
   category: "gioielli",
+  image: "https://marlu-18ddd.kxcdn.com/4983-square_large_default/collana-medaglietta-sole-pendente-con-zirconi.jpg",
   links: [{url: "https://www.marlu.it/it/collane/923-collana-medaglietta-sole-pendente-con-zirconi.html?_gl=1*py9yx2*_up*MQ..*_ga*OTM4Njc1MDk5LjE3NjY5NjA1NDA.*_ga_8ETQBCRR5Q*czE3NjY5NjA1MzkkbzEkZzEkdDE3NjY5NjE3NDIkajYwJGwwJGgw", label:"Marlù"}],
   size: null,
   color: null,
@@ -77,6 +83,7 @@ export const items = [
   name: "Collarino rigido",
   price: 29,
   category: "gioielli",
+  image: "https://marlu-18ddd.kxcdn.com/1538-square_large_default/collarino-rigido.jpg",  
   links: [{url: "https://www.marlu.it/it/collane/340-1189-collarino-rigido.html?_gl=1*1l274do*_up*MQ..*_ga*OTM4Njc1MDk5LjE3NjY5NjA1NDA.*_ga_8ETQBCRR5Q*czE3NjY5NjA1MzkkbzEkZzEkdDE3NjY5NjE3NDIkajYwJGwwJGgw#/160-colore-oro/177-materiale-pvd_oro", label:"Marlù"}],
   size: null,
   color: null,
@@ -88,6 +95,7 @@ export const items = [
   name: "Bracciale sole",
   price: 39,
   category: "gioielli",
+  image: "https://marlu-18ddd.kxcdn.com/4197-square_large_default/bracciale-sole.jpg",  
   links: [{url: "https://www.marlu.it/it/bracciali/742-2572-bracciale-sole.html?_gl=1*1tfflnu*_up*MQ..*_ga*OTM4Njc1MDk5LjE3NjY5NjA1NDA.*_ga_8ETQBCRR5Q*czE3NjY5NjA1MzkkbzEkZzEkdDE3NjY5NjI1MDYkajYwJGwwJGgw#/88-taglia-regolabile_da_14_a_18_cm/169-colore-bianco/177-materiale-pvd_oro", label:"Marlù"}],
   size: null,
   color: "dorato",
@@ -99,6 +107,7 @@ export const items = [
   name: "Bracciale doppia catena snake e grumetta con sfere",
   price: 18,
   category: "gioielli",
+  image: "https://marlu-18ddd.kxcdn.com/23242-square_large_default/bracciale-doppia-catena-snake-e-grumetta-con-sfere.jpg",  
   links: [{url: "https://www.marlu.it/it/bracciali/1803-24287-bracciale-doppia-catena-snake-e-grumetta-con-sfere.html?_gl=1*1fkf28y*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzAkdDE3NjY5NjI1ODkkajYwJGwwJGgw#/121-taglia-regolabile_da_15_a_19_cm/160-colore-oro/177-materiale-pvd_oro", label:"Marlù"}],
   size: null,
   color: "dorato",
@@ -110,6 +119,7 @@ export const items = [
   name: "Orecchino a lobo con zircone 5mm",
   price: 14,
   category: "gioielli",
+  image: "https://marlu-18ddd.kxcdn.com/7041-square_large_default/orecchino-a-lobo-con-zircone-5mm.jpg",  
   links: [{url: "https://www.marlu.it/it/orecchini/1456-3938-orecchino-a-lobo-con-zircone-5mm.html?_gl=1*85xnsz*_up*MQ..*_ga*MjA0MzMyMDE0NS4xNzY2OTYyNTg5*_ga_8ETQBCRR5Q*czE3NjY5NjI1ODkkbzEkZzEkdDE3NjY5NjI3OTckajYwJGwwJGgw#/132-taglia-diametro_5mm/169-colore-bianco/177-materiale-pvd_oro", label: "Marlù"}],
   size: null,
   color: "dorato",
